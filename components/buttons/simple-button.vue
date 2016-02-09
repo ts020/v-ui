3bb87feac2cd5052c2cfc022bc168c92c9f88f1c
菅原孝則 <takanori.sugawara@gmail.com>
2016-02-09 16:49:24 +0900

    add simple-button

---
+++ components/buttons/simple-button.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="simple-button"><slot></slot></div>
+</template>
+<!--suppress CssInvalidPropertyValue -->
+<style scoped lang="stylus">
+    @import "../../style/_val.styl";
+    .simple-button {
+        padding: 15px 30px;
+        margin: 0 10px;
+        display: inline-block;
+        border-radius: 30px;
+        border: 2px solid main-color;
+        color: main-color;
+        background-color: white;
+        line-height: 1em;
+        font-size: 14px;
+        cursor: pointer;
+        transition-property: all;
+        transition-duration: 0.15s;
+        transition-timing-function: ease-in-out;
+    }
+
+    .simple-button:hover {
+        background-color: main-color;
+        color : white;
+        padding: 15px 40px;
+        margin: 0 0;
+    }
+</style>
--- src/pages/buttons-page.vue
+++ src/pages/buttons-page.vue
@@ -2,8 +2,8 @@
 <div class="buttons-page">
     <page-base>
         <h1 slot="title" class="display-1">Buttons EXAMPLE</h1>
-        <button>ボタン</button>
-        <button>ボタン2</button>
+        <simple-button>ボタン</simple-button>
+        <simple-button>Submit</simple-button>
     </page-base>
 </div>
 </template>
@@ -15,7 +15,8 @@
 <script lang="babel" type="text/ecmascript-6">
     export default {
         components : {
-            "page-base" : require("./page-base.vue")
+            "page-base" : require("./page-base.vue"),
+            "simple-button" : require("../../components/buttons/simple-button.vue")
         }
     }
 </script>
